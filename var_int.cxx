027c3ec603ddab5e58eaeb954804e4c79712b41d
Adam <adam@eseabrook.co.uk>
2014-07-02 23:16:35 +0100

    filled out the constructor for var_int

--- var_int.cxx
+++ var_int.cxx
@@ -2,5 +2,40 @@
 
 var_int::var_int (const uint64_t n)
 {
+  if (n < 0xFD)
+  {
+    data = (char*) malloc(1);
+    data[0] = n & 0xFF;
+  } else if (n < 0xFFFF)
+  {
+    // allocate 3 bytes
+    data = (char*) malloc(3);
 
+    data[0] = 0xFE;
+    data[1] = n & 0xFF;
+    data[2] = (n  >> 8) & 0xFF;
+
+  } else if (n < 0xFFFFFFFF)
+  {
+    // allocate 5 bytes
+    data = (char*) malloc(5);
+
+    data[0] = 0xFE;
+    
+    for (int i = 0; i < 4; i++)
+    {
+      data[i + 1] = (n >> (i * 8)) & 0xFF;
+    }
+  } else
+  {
+    // allocate 9 bytes
+    data = (char*) malloc(9);
+
+    data[0] = 0xFF;
+
+    for (int i = 0; i < 8; i++)
+    {
+      data[i + 1] = (n >> (i * 8)) & 0xFF;
+    }
+  }
 }
