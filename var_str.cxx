bea4d6bfcb8d0c998ced3b756f9036a587287075
Alex <ard.bates@gmail.com>
2014-07-02 22:34:08 +0100

    changed var_str to compile

--- var_str.cxx
+++ var_str.cxx
@@ -3,7 +3,7 @@
 using namespace std;
 
 var_str::var_str (const string str)
-  : length (var_int::to_var_int (str.length ())) 
+  : length (var_int (str.length ()))
 {
   this->str = new char[str.length ()]; 
   memcpy (this->str, str.c_str (), str.length ()); /* this avoids copying the null-termiantor */ 
